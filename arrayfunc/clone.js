7a0c515ee8b79e93e3ca7d2593e21254c437770c
yzq <yzqdev@outlook.com>
2020-10-23 14:19:09 +0800

    update

--- arrayfunc/clone.js
+++ arrayfunc/clone.js
@@ -15,7 +15,7 @@ function deepCopy(obj) {
   //判断是否是简单数据类型，
   if (typeof obj == "object") {
     //复杂数据类型
-    var result = obj.constructor == Array ? [] : {};
+    var result = obj.constructor === Array ? [] : {};
     for (let i in obj) {
       result[i] = typeof obj[i] == "object" ? deepCopy(obj[i]) : obj[i];
     }
--- debugmodel/app.js
+++
@@ -1,9 +0,0 @@
-const log = require('debug')('auth');
-
-//creates new debug instance with extended namespace
-const logSign = log.extend('sign');
-const logLogin = log.extend('login');
-
-log('hello'); // auth hello
-logSign('hello'); //auth:sign hello
-logLogin('hello'); //auth:login hello
